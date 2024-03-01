fbd1f45136f0fbf419c7e76bde7ed695d066d90d
Robbie Bagley <rbagley01@gmail.com>
2024-02-29 22:55:39 -0500

    Structure complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import MainNav from '@/components/MainNav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className='flex flex-col items-center border-b mb-5 px-5 py-3'>
+          <div className='max-w-6xl w-full'>
+            <MainNav />
+          </div>
+        </nav>
+        <main className='flex flex-col items-center border'>
+          <div className='max-w-6xl w-full'>
+            {children}
+          </div>
+        </main></body>
     </html>
   )
 }
